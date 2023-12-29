e27c245edcb447cb789b9da69d234740de2699a7
CRING-PENJOR <155144886+CRING-PENJOR@users.noreply.github.com>
2023-12-29 15:52:28 +0600

    Add files via upload

---
+++ personal portfolio/java/portfolio.js
@@ -0,0 +1,31 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+ 
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function(){
+            startValue += 1;
+            valueDisplay.textContent = startValue;
+            if (startValue == endValue) {
+                clearInterval(counter);
+            }
+        }, duration);
+    });
+
+
+    function myFunction(){
+        var x = document.getElementById("myTopmenu");
+        if(x.className === "navbar"){
+            x.className += " responsive";
+        }
+        else{
+            x.className = "navbar";
+        }
+        }
+
+
+
+       
